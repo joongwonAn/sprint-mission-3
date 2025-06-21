23078616ca5f814b19b1fc0e4a0779fe49c2b727
joongwon <anjoongwon517@gmail.com>
2025-06-21 10:30:18 +0900

    Bean 선언 및 테스트

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
@@ -4,6 +4,7 @@ import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.ChannelType;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,12 +12,9 @@ import java.util.NoSuchElementException;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class BasicChannelService implements ChannelService {
     private final ChannelRepository channelRepository;
-
-    public BasicChannelService(ChannelRepository channelRepository) {
-        this.channelRepository = channelRepository;
-    }
 
     @Override
     public Channel create(ChannelType type, String name, String description) {
